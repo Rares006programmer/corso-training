b6015395d262784becc218296245810586970b84
Rares <orbanrares06@gmail.com>
2024-08-14 00:09:59 +0300

    Made a few overall adjustments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Add custom JavaScript here
+const forms = document.querySelectorAll(".needs-validation");
+console.log(forms);
+
+forms.forEach((form) => {
+	form.addEventListener("submit", (event) => {
+		if (!form.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		form.classList.add("was-validated");
+	});
+});
